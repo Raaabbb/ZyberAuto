2d8aafa7bd5e68c2616e7ed53b2c41309d2a69a5
Raaabbb <gleomir28@gmail.com>
2026-02-03 10:21:55 +0800

    Update MainActivity

--- app/src/main/java/com/example/zyberauto/MainActivity.kt
+++ app/src/main/java/com/example/zyberauto/MainActivity.kt
@@ -7,7 +7,6 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import com.example.zyberauto.presentation.navigation.AppNavigation
 import com.example.zyberauto.ui.theme.ZyberAutoTheme
@@ -15,18 +14,16 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        android.util.Log.d("DEBUG_MA", "MainActivity onCreate started")
         enableEdgeToEdge()
         setContent {
-            android.util.Log.d("DEBUG_MA", "setContent called - Composing UI")
             ZyberAutoTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                   android.util.Log.d("DEBUG_MA", "Surface content - AppNavigation")
                    AppNavigation()
                 }
             }
